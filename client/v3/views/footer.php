6a06c76431d37c734de66dbbaeb1e6025e8a6548
JordyCheco <112654260+JordyCheco@users.noreply.github.com>
2023-03-18 14:35:29 -0400

    created a footer and working on a carousel

---
+++ client/v3/views/footer.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="../css/main.css">
+    <title>SUNY New Paltz Children's Center</title>
+
+</head>
+
+<!--------------------- Footer start  ---------------------> 
+<body>
+    <footer class="bg-gray-100">
+        <div class="container mx-auto p-8">
+            <div class="flex flex-col md:flex-row justify-between">
+                <div class="flex flex-col space-y-4">
+                    <h3 class="text-xl font-bold">Contact Us</h3>
+                    <p class="text-gray-500">Phone: (845) 257-3000</p>
+                    <p class="text-gray-500">Address: 1 Hawk Drive, New Paltz, NY 12561</p>
+                </div>
+
+    
+            </div>
+        </div>
+    </footer>
+</body>
+<!--------------------- Footer end  --------------------->
