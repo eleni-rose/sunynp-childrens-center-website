20a42912884d818f9bd8132bad7e20dbcbdf8584
kelpo <eloonku@gmail.com>
2023-03-20 00:38:59 -0400

    Some small changes to email function

--- client/v3/server/email/send-email.php
+++ client/v3/server/email/send-email.php
@@ -1,41 +1,43 @@
 <?php
 
+// Global variables obtained from input in contact.html
 $name = $_POST["name"];
 $email = $_POST["email"];
 $subject = $_POST["subject"];
 $message = $_POST["message"];
 
-
 // Note - need to include the PHPMailer files for things to actually work!
-
 use PHPMailer\PHPMailer\PHPMailer;
 require('SMTP.php');
-require('PHPMailer.php');
 require('Exception.php');
+require('PHPMailer.php');
 
 $mail = new PHPMailer(true);
 
-$mail->isSMTP();
-$mail->SMTPAuth = true;
-$mail->SMTPDebug  = 1;
+try {
 
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
+    // SMTP server settings
+    $mail->isSMTP();
+    $mail->SMTPAuth = true;
+    $mail->SMTPDebug  = 1;
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+    $mail->Username = "hothotspicy69@gmail.com";
+    $mail->Password = "flnnkqrwhlgietbv";
 
-$mail->Username = "secret";
-$mail->Password = "secret";
+    // SMTP recipients
+    $mail->setFrom('contact@example.net', $name);
+    $mail->addReplyTo($email, $name);
+    $mail->addAddress("eloonku@gmail.com", 'Eleni');
 
-$mail->setFrom($email, $name);
+    // Email content
+    $mail->Subject = $subject;
+    $mail->Body = $message;
 
-// Email to be sent to
-$mail->addAddress("eloonku@gmail.com");
+    $mail->send();
+    header("Location: sent.html");
 
-$mail->Subject = $subject;
-$mail->Body = $message;
-
-$mail->send();
-
-header("Location: sent.html");
-
-?>
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
