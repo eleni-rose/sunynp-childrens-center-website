6a4a9696df925b8f8c36e6ee27e87b2ee9619284
kelpo <eloonku@gmail.com>
2023-03-24 04:34:19 -0400

    Working sign up form

--- client/v3/views/header.php
+++ client/v3/views/header.php
@@ -20,6 +20,7 @@
         <!--------------------- Navigation items start  --------------------->
 
             <div class="text-xl text-center hidden md:flex space-x-8">
+
                 <a href="/" class="my-1 group transition duration-500">
                     Home
                     <span
@@ -118,70 +119,6 @@
     </nav>
 
 <!--------------------- Navbar end  --------------------->
-
-<!--------------------- Banner carousel start  --------------------->
-
-    <section id="banner-carousel">
-        <div class="w-full bg-cover bg-center" style="height:38rem; background-image: url('../img/banner4.jpg');">
-            <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50 hover:bg-opacity-90 transition duration-700">
-
-            <!--------------------- Banner text and items start  --------------------->
-                <div class="text-center">
-
-                    <h1 class="text-white text-5xl font-bold uppercase">
-                        Welcome to the <span class="text-orange-400">SUNY New Paltz</span> children's center!
-                    </h1>
-
-                    <h2 class="text-white text-4xl">
-                        Dedicated to serving the New Paltz community since 19xx
-                    </h2>
-
-                </div>
-
-            <!--------------------- Banner text and items end  --------------------->
-            </div>
-        </div>
-    </section>
-
-<!--------------------- Banner carousel end --------------------->
-
-<!--------------------- Information section start --------------------->
-
-    <section id="information">
-        <div class="flex flex-row p-10 m-10">
-
-        <!--------------------- Information items start --------------------->
-
-            <div>
-                <h1 class="text-4xl text-center bg-slate-300 m-5 p-5">Our Promise<h1>
-
-                <h2 class="text-2xl text-center m-5 p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Totam expedita, suscipit cumque natus distinctio magni eligendi amet?
-                    Quis sit consequatur veritatis rem. Nemo aspernatur beatae non dicta magni natus saepe?</h2>
-            </div>
-
-            <div>
-                <h1 class="text-4xl text-center bg-slate-200 m-5 p-5">Our Promise<h1>
-
-                <h2 class="text-2xl text-center m-5 p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Totam expedita, suscipit cumque natus distinctio magni eligendi amet?
-                    Quis sit consequatur veritatis rem. Nemo aspernatur beatae non dicta magni natus saepe?</h2>
-            </div>
-
-            <div>
-                <h1 class="text-4xl text-center bg-slate-300 m-5 p-5">Our Promise<h1>
-
-                <h2 class="text-2xl text-center m-5 p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Totam expedita, suscipit cumque natus distinctio magni eligendi amet?
-                    Quis sit consequatur veritatis rem. Nemo aspernatur beatae non dicta magni natus saepe?</h2>
-            </div>
-
-        <!--------------------- Information items end --------------------->
-
-        </div>
-    </section>
-
-<!--------------------- Information section end --------------------->
 
 </body>
 </html>
--- client/v3/views/index.php
+++ client/v3/views/index.php
@@ -8,7 +8,73 @@
   <link href="https://fonts.googleapis.com/css2?family=Odibee+Sans&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
 </head>
+
+<!--------------------- Banner carousel start  --------------------->
 <body>
+
+<section id="banner-carousel">
+        <div class="w-full bg-cover bg-center" style="height:38rem; background-image: url('../img/banner4.jpg');">
+            <div class="flex items-center justify-center h-full w-full bg-gray-900 bg-opacity-50 hover:bg-opacity-90 transition duration-700">
+
+            <!--------------------- Banner text and items start  --------------------->
+                <div class="text-center">
+
+                    <h1 class="text-white text-5xl font-bold uppercase">
+                        Welcome to the <span class="text-orange-400">SUNY New Paltz</span> children's center!
+                    </h1>
+
+                    <h2 class="text-white text-4xl">
+                        Dedicated to serving the New Paltz community since 19xx
+                    </h2>
+
+                </div>
+
+            <!--------------------- Banner text and items end  --------------------->
+            </div>
+        </div>
+    </section>
+
+<!--------------------- Banner carousel end --------------------->
+
+<!--------------------- Information section start --------------------->
+
+    <section id="information">
+        <div class="flex flex-row p-10 m-10">
+
+        <!--------------------- Information items start --------------------->
+
+            <div>
+                <h1 class="text-4xl text-center bg-slate-300 m-5 p-5">Our Promise<h1>
+
+                <h2 class="text-2xl text-center m-5 p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Totam expedita, suscipit cumque natus distinctio magni eligendi amet?
+                    Quis sit consequatur veritatis rem. Nemo aspernatur beatae non dicta magni natus saepe?</h2>
+            </div>
+
+            <div>
+                <h1 class="text-4xl text-center bg-slate-200 m-5 p-5">Our Promise<h1>
+
+                <h2 class="text-2xl text-center m-5 p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Totam expedita, suscipit cumque natus distinctio magni eligendi amet?
+                    Quis sit consequatur veritatis rem. Nemo aspernatur beatae non dicta magni natus saepe?</h2>
+            </div>
+
+            <div>
+                <h1 class="text-4xl text-center bg-slate-300 m-5 p-5">Our Promise<h1>
+
+                <h2 class="text-2xl text-center m-5 p-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Totam expedita, suscipit cumque natus distinctio magni eligendi amet?
+                    Quis sit consequatur veritatis rem. Nemo aspernatur beatae non dicta magni natus saepe?</h2>
+            </div>
+
+        <!--------------------- Information items end --------------------->
+
+        </div>
+    </section>
+
+<!--------------------- Information section end --------------------->
+
+<!--
   <h1>Pure <span id="css">Css</span> Carousel</h1>
   <div class="carousel-wrapper">
     <span id="item-1"></span>
@@ -30,5 +96,7 @@
     </div>
 
   </div>
+
+-->
 </body>
 </html>
