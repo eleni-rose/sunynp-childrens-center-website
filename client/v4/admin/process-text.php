21acc0e8eaddb4361eab87243c883e637aa9b4f6
Eleni <eloonku@gmail.com>
2023-04-18 15:14:56 -0400

    Dynamically change database text records via ID

---
+++ client/v4/admin/process-text.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . "/config.php";
+
+$id = $_POST["id"];
+$content = $_POST["content"];
+$info = $_POST["info"];
+
+$sql = "UPDATE text 
+        SET content = '$content', info = '$info'
+        WHERE id='$id'";
+
+$stmt = $mysqli->stmt_init();
+
+if (! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+if ($stmt->execute()) {
+    header("Location: update-success.html");
+    exit;
+} else {
+    
+    die($mysqli-->error . " ". $mysqli->errno);
+
+}
