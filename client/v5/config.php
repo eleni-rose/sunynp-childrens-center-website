175bff0e6eb07ce54e2fce1d05f305f9e62a0e9e
Eleni <eloonku@gmail.com>
2023-10-18 23:23:35 -0400

    Updated config.php

--- client/v5/config.php
+++ client/v5/config.php
@@ -2,9 +2,9 @@
 
 // Database credentials
 $host = "localhost";
-$username = "p_s23_04";
-$password = "dtru0x";
-$dbname = "p_s23_04_db";
+$username = "secret";
+$password = "secret";
+$dbname = "secret";
 
 // Attempt to connect to MySQL database
 $mysqli = new mysqli($host, $username, $password, $dbname);
